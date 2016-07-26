ea405e5290a1c55cdfeb1f459b4f6fa925325d5c
Dennis Ahlqvist <dennisahlqvist@hotmail.com>
2016-07-26 17:07:40 +0300

    no message

--- test/token/vote-test.js
+++ test/token/vote-test.js
@@ -3,7 +3,7 @@ var async = require('async');
 var Sandbox = require('ethereum-sandbox-client');
 var helper = require('ethereum-sandbox-helper');
 
-describe('Buy tokens', function() {
+describe('Vote Contract', function() {
   this.timeout(60000);
   
   var alice = '0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826';
@@ -33,33 +33,33 @@ describe('Buy tokens', function() {
       }
     );
   });
-  
-  it('Buy tokens', function(done) {
-    var value = 100111;
-    var balanceBefore = sandbox.web3.eth.getBalance(alice);
-    
-    assert(token.balances(alice).eq(0), 'Alice balance is not zero');
-    sandbox.web3.eth.sendTransaction({
-      from: alice,
-      to: token.address,
-      value: value
-    }, function(err, txHash) {
-      if (err) return done(err);
-      helper.waitForReceipt(sandbox.web3, txHash, function(err, receipt) {
-        if (err) return done(err);
-        
-        var tokens = Math.floor(value / price);
-        assert(token.balances(alice).eq(tokens), 'Account has not get ' + tokens + ' tokens');
-        
-        var txCost = sandbox.web3.eth.gasPrice.mul(receipt.gasUsed);
-        var spent = tokens * price;
-        var expectedBalance = balanceBefore.sub(txCost).sub(spent);
-        assert(sandbox.web3.eth.getBalance(alice).eq(expectedBalance), 'Account balance is not correct');
-        
-        done();
-      });
-    });
-  });
+
+  // it('Buy tokens', function(done) {
+  //   var value = 100111;
+  //   var balanceBefore = sandbox.web3.eth.getBalance(alice);
+  //
+  //   assert(token.balances(alice).eq(0), 'Alice balance is not zero');
+  //   sandbox.web3.eth.sendTransaction({
+  //     from: alice,
+  //     to: token.address,
+  //     value: value
+  //   }, function(err, txHash) {
+  //     if (err) return done(err);
+  //     helper.waitForReceipt(sandbox.web3, txHash, function(err, receipt) {
+  //       if (err) return done(err);
+  //
+  //       var tokens = Math.floor(value / price);
+  //       assert(token.balances(alice).eq(tokens), 'Account has not get ' + tokens + ' tokens');
+  //
+  //       var txCost = sandbox.web3.eth.gasPrice.mul(receipt.gasUsed);
+  //       var spent = tokens * price;
+  //       var expectedBalance = balanceBefore.sub(txCost).sub(spent);
+  //       assert(sandbox.web3.eth.getBalance(alice).eq(expectedBalance), 'Account balance is not correct');
+  //
+  //       done();
+  //     });
+  //   });
+  // });
 
   it('Bob vote yes test', function(done) {
     // var bobValue = 50000;
@@ -107,27 +107,27 @@ describe('Buy tokens', function() {
       });
     }
   });
-  
-  it('Withdraw', function(done) {
-    var tokensBefore = token.balances(alice).toNumber();
-    var tokensWithdraw = 100;
-    var balanceBefore = sandbox.web3.eth.getBalance(alice);
-    
-    token.withdraw(tokensWithdraw, { from: alice }, function(err, txHash) {
-      if (err) return done(err);
-      helper.waitForReceipt(sandbox.web3, txHash, function(err, receipt) {
-        if (err) return done(err);
-        
-        assert(token.balances(alice).eq(tokensBefore - tokensWithdraw), 'Alice tokens balance is not correct');
 
-        var txCost = sandbox.web3.eth.gasPrice.mul(receipt.gasUsed);
-        var expectedBalance = balanceBefore.sub(txCost).add(tokensWithdraw * price);
-        assert(sandbox.web3.eth.getBalance(alice).eq(expectedBalance), 'Alice balance is not correct');
-
-        done();
-      });
-    });
-  });
+  // it('Withdraw', function(done) {
+  //   var tokensBefore = token.balances(alice).toNumber();
+  //   var tokensWithdraw = 100;
+  //   var balanceBefore = sandbox.web3.eth.getBalance(alice);
+  //
+  //   token.withdraw(tokensWithdraw, { from: alice }, function(err, txHash) {
+  //     if (err) return done(err);
+  //     helper.waitForReceipt(sandbox.web3, txHash, function(err, receipt) {
+  //       if (err) return done(err);
+  //
+  //       assert(token.balances(alice).eq(tokensBefore - tokensWithdraw), 'Alice tokens balance is not correct');
+  //
+  //       var txCost = sandbox.web3.eth.gasPrice.mul(receipt.gasUsed);
+  //       var expectedBalance = balanceBefore.sub(txCost).add(tokensWithdraw * price);
+  //       assert(sandbox.web3.eth.getBalance(alice).eq(expectedBalance), 'Alice balance is not correct');
+  //
+  //       done();
+  //     });
+  //   });
+  // });
 
   after(function(done) {
     sandbox.stop(done);
